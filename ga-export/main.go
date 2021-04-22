82530800199142c02d9e653ea4538f26268cd6a0
Jon Penn <jpenn@bentonvillek12.org>
2021-04-22 08:31:58 -0500

    change exported Google Admin fields

--- ga-export/main.go
+++ ga-export/main.go
@@ -16,37 +16,47 @@ func main() {
 	// get authorized google admin client (may prompt for oauth flow)
 	ga := common.GAdmin()
 
-	// get predictions that are not already reflected in the admin console
+	// get devices in the admin console where the user or asset_id field
+	// does not match the value we would assign it
 	rows, err := db.Query(`
 		SELECT
 			predictions.device_id,
-			predictions.user
-		FROM predictions
-		LEFT JOIN chromebooks
-			ON predictions.device_id = chromebooks.device_id
+			COALESCE(predicted_users.first_name || ' ' || predicted_users.last_name || ' <' || predictions.user || '>', 'No recent student logins') AS "user",
+			COALESCE(assigned_users.first_name || ' ' || assigned_users.last_name || ' <' || assets.client || '>', 'Not assigned') AS client
+		FROM chromebooks
+		LEFT JOIN predictions
+			ON chromebooks.device_id = predictions.device_id
+		LEFT JOIN users AS predicted_users
+			ON predictions.user = predicted_users.email
+		LEFT JOIN assets
+			ON chromebooks.serial = assets.serial
+		LEFT JOIN users AS assigned_users
+			ON assets.client = assigned_users.email
 		WHERE
-			predictions.user <> chromebooks.asset_id OR
-			chromebooks.asset_id IS NULL
+			chromebooks.user <> COALESCE(predicted_users.first_name || ' ' || predicted_users.last_name || ' <' || predictions.user || '>', 'No recent student logins') OR
+			chromebooks.asset_id <> COALESCE(assigned_users.first_name || ' ' || assigned_users.last_name || ' <' || assets.client || '>', 'Not assigned')
 	`)
 	jgh.PanicOnErr(err)
 	defer rows.Close()
 
 	for rows.Next() {
-		var deviceID, user string
-		err = rows.Scan(&deviceID, &user)
+		var deviceID, user, client string
+		err = rows.Scan(&deviceID, &user, &client)
 		jgh.PanicOnErr(err)
 
-		fmt.Printf("%s is now owned by %s\n", deviceID, user)
+		fmt.Printf("Updateing %s\n", deviceID)
 
-		err = setAssetId(deviceID, user, ga)
+		err = setCustomFields(deviceID, user, client, ga)
 		jgh.PanicOnErr(err)
 
 		// update admin console table to reflect the change we just made
 		_, err = db.Exec(`
 			UPDATE chromebooks
-			SET asset_id = $1
-			WHERE device_id = $2
-		`, user, deviceID)
+			SET
+				"user" = $1,
+				asset_id = $2
+			WHERE device_id = $3
+		`, user, client, deviceID)
 		jgh.PanicOnErr(err)
 
 		// google gets mad if we go too fast
@@ -55,12 +65,13 @@ func main() {
 	jgh.PanicOnErr(rows.Err())
 }
 
-func setAssetId(deviceID, newAssetID string, ga *admin.Service) error {
+func setCustomFields(deviceID, user, asset string, ga *admin.Service) error {
 	_, err := ga.Chromeosdevices.Patch(
 		"my_customer",
 		deviceID,
 		&admin.ChromeOsDevice{
-			AnnotatedAssetId: newAssetID,
+			AnnotatedUser:    user,
+			AnnotatedAssetId: asset,
 		},
 	).Do()
 	return err
