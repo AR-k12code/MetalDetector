0f5069be64d4968a51a8b0c6531ca765f7826072
Jon Penn <jpenn@bentonvillek12.org>
2021-05-21 17:04:09 -0500

    working generalized kiosk

--- kiosk/background.js
+++ kiosk/background.js
@@ -21,12 +21,15 @@ chrome.alarms.get(alarm => {
 function sessionStart() {
 	chrome.storage.local.set({ sessionStart: timestamp() });
 	console.log('session start');
+	sendReport();
 }
 
 chrome.runtime.onStartup.addListener(sessionStart);
 chrome.runtime.onInstalled.addListener(sessionStart);
 
-chrome.alarms.onAlarm.addListener(() => {
+chrome.alarms.onAlarm.addListener(sendReport);
+
+function sendReport() {
 	chrome.storage.managed.get('server', settings =>
 		reportData(data =>
 			fetch(settings.server, {
@@ -39,52 +42,51 @@ chrome.alarms.onAlarm.addListener(() => {
 		)
 	);
 	console.log('send report');
-});
+}
 
 function reportData(callback) {
 	chrome.enterprise.deviceAttributes.getDeviceSerialNumber(serial =>
-		chrome.enterprise.networkingAttributes.getNetworkDetails(net =>
-			chrome.storage.managed.get({ maxGeoAge: 300 }, settings =>
-				chrome.storage.local.get('sessionStart', storage =>
-					navigator.geolocation.getCurrentPosition(geo => {
-						callback({
-							timestamp: timestamp(),
-							email: 'KIOSK',
-							serial: serial,
-							ipv4: net.ipv4,
-							ipv6: net.ipv6,
-							geoTimestamp: Math.floor(geo.timestamp / 1000),
-							latitude: geo.coords.latitude,
-							longitude: geo.coords.longitude,
-							accuracy: geo.coords.accuracy,
-							sessionStart: storage.sessionStart
-						});
-					}, err => {
-						callback({
-							timestamp: timestamp(),
-							email: 'KIOSK',
-							serial: serial,
-							ipv4: net.ipv4,
-							ipv6: net.ipv6,
-							sessionStart: storage.sessionStart,
-						});
-						console.log('geolocation failed', err);
-					}, {
-						enableHighAccuracy: true,
-						timeout: 10 * 1000,
-						maximumAge: settings.maxGeoAge * 1000
-					})
-				)
+		chrome.storage.managed.get({ maxGeoAge: 300 }, settings =>
+			chrome.storage.local.get('sessionStart', storage =>
+				navigator.geolocation.getCurrentPosition(geo => {
+					callback({
+						timestamp: timestamp(),
+						email: 'KIOSK',
+						serial: serial,
+						geoTimestamp: Math.floor(geo.timestamp / 1000),
+						latitude: geo.coords.latitude,
+						longitude: geo.coords.longitude,
+						accuracy: geo.coords.accuracy,
+						sessionStart: storage.sessionStart
+					});
+				}, err => {
+					callback({
+						timestamp: timestamp(),
+						email: 'KIOSK',
+						serial: serial,
+						sessionStart: storage.sessionStart,
+					});
+					console.log('geolocation failed', err);
+				}, {
+					enableHighAccuracy: true,
+					timeout: 10 * 1000,
+					maximumAge: settings.maxGeoAge * 1000
+				})
 			)
 		)
 	);
 }
 
-chrome.app.runtime.onLaunched.addListener(() => {
-	// get the device serial, save it, then open a window
-	chrome.enterprise.deviceAttributes.getDeviceSerialNumber(serial =>
-		chrome.storage.local.set({ "serial": serial }, () => 
-			chrome.app.window.create('window.html')
+chrome.app.runtime.onLaunched.addListener(() =>
+	chrome.storage.managed.get('display', settings =>
+		chrome.enterprise.deviceAttributes.getDeviceSerialNumber(serial =>
+			chrome.storage.local.set(
+				{
+					'serial': serial,
+					'display': settings.display
+				},
+				() => chrome.app.window.create('window.html')
+			)
 		)
-	);
-});
+	)
+);
